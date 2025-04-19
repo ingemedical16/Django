25f57eb66de7916605b1f5c3e677ff9cd4a6d448
ingemedical16 <ingmedical16@gmail.com>
2025-04-19 09:59:12 +0000

    Adding more Views & urls

--- 004_url_view_mapper/monthley_challenges/challenges/urls.py
+++ 004_url_view_mapper/monthley_challenges/challenges/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("january", views.index),
+    path("", views.index),
+    path("january", views.january),
+    path("february", views.february),
 ]
