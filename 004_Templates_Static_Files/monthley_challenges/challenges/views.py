a07de13e9f3f2c85fae4bc99cfa2335df56e7708
ingemedical16 <ingmedical16@gmail.com>
2025-04-20 09:15:22 +0000

    rendring Templates

--- 004_Templates_Static_Files/monthley_challenges/challenges/views.py
+++ 004_Templates_Static_Files/monthley_challenges/challenges/views.py
@@ -42,7 +42,7 @@ def monthley_challenge_by_number(request, month):
 def monthly_challenge(request, month):
     try:
         challenge_text = monthley_challenges[month]
-        response_data = render_to_string("challenges/challenge.html")
-        return HttpResponse(response_data)
+        return render(request,"challenges/challenge.html")
+      
     except:
         return HttpResponseNotFound("<h1>This month is not supported!</h1>")
