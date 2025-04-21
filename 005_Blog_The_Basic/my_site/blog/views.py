5685650b47808aa4f044c66c8a9a738d69d5fb33
ingemedical16 <ingmedical16@gmail.com>
2025-04-21 15:35:52 +0000

    Adding URLs & Views

--- 005_Blog_The_Basic/my_site/blog/views.py
+++ 005_Blog_The_Basic/my_site/blog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
+
+def posts(request):
+    return HttpResponse("Hello, world. You're at Posts Page")
+
+def post_detail(request,slug):
+    return HttpResponse("Hello, world. You're at Post Detail Page " +slug)
