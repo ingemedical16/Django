7b1031975ea3e3343919fd49485508d29d55cdce
ingemedical16 <ingmedical16@gmail.com>
2025-04-21 18:29:56 +0000

    Special Template Features & Syntax

--- 005_Blog_The_Basic/my_site/blog/views.py
+++ 005_Blog_The_Basic/my_site/blog/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 from datetime import date
 
-posts = [
+all_posts = [
     {
         "slug": "hike-in-the-mountains",
         "image": "mountains.jpg",
-ƒ        "author": "Mohammed EL-IDRISSI",
+       "author": "Mohammed EL-IDRISSI",
         "date": date(2021, 7, 21),
         "title": "Mountain Hiking",
         "excerpt": "There's nothing like the views you get when hiking in the mountains! And I wasn't even prepared for what happened whilst I was enjoying the view!",
@@ -67,10 +67,17 @@ posts = [
     }
 ]
 
+def get_date(post):
+    return post['date']
+
 # Create your views here.
 
 def index(request):
-    return render(request, 'blog/index.html')
+    sorted_posts = sorted(all_posts, key=get_date)
+    latest_posts = sorted_posts[-3:]
+    return render(request, 'blog/index.html',{
+        "posts": latest_posts
+    })
 
 def posts(request):
     return render(request, 'blog/all-posts.html')
