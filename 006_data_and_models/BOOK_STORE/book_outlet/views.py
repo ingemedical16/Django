62593a80bc46f9c808163ad08bbb22e4f4bd8e23
Mohammed EL-IDRISSI <ingmedical16@gmail.com>
2025-06-29 16:58:09 +0200

    Rendering the Details Page

--- 006_data_and_models/BOOK_STORE/book_outlet/views.py
+++ 006_data_and_models/BOOK_STORE/book_outlet/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
 
 from .models import Book
 
@@ -9,3 +10,16 @@ def index(request):
    return render(request, "book_outlet/index.html", {
       "books": books
    }) 
+
+def book_detail(request,id):
+   # try:
+   #   book = Book.objects.get(pk=id)
+   #except:
+   #   raise Http404()
+   book = get_object_or_404(Book, pk=id)
+   return render(request,"book_outlet/book_detail.html", {
+      "title": book.title,
+      "author": book.author,
+      "rating": book.rating,
+      "is_bestseller": book.is_bestselling
+   })
