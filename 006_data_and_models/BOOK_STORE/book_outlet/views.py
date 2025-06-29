71640bc941b6b84d59befa329d457e9b192103b2
Mohammed EL-IDRISSI <ingmedical16@gmail.com>
2025-06-29 16:22:07 +0200

    Rendering Queried Data in the Template

--- 006_data_and_models/BOOK_STORE/book_outlet/views.py
+++ 006_data_and_models/BOOK_STORE/book_outlet/views.py
@@ -6,4 +6,6 @@ from .models import Book
 
 def index(request):
    books = Book.objects.all()
-   return render(request, "book_outlet/index.html") 
+   return render(request, "book_outlet/index.html", {
+      "books": books
+   }) 
