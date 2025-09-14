c8b72e72c3a3324112e5a0c28e7bcdd9f63ee2d3
Mohammed El-IDRISSI <ingmedical16@gmail.com>
2025-09-14 12:40:16 +0000

    Customizing the Form Controls

--- 010_Forms/reviews/forms.py
+++ 010_Forms/reviews/forms.py
@@ -1,4 +1,7 @@
 from django import forms
 
 class ReviewFrom(forms.Form):
-    user_name = forms.CharField()
+    user_name = forms.CharField(label="Your Name", max_length=100, error_messages={
+        "required": "Your name must not be empty",
+        "max_length":"Plase enter a shorter name!"
+    })
