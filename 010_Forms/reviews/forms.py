f5b946b821b1d979020b16ae3dca467f203872b1
Mohammed El-IDRISSI <ingmedical16@gmail.com>
2025-09-14 14:21:22 +0000

    Introducing Modelforms

--- 010_Forms/reviews/forms.py
+++ 010_Forms/reviews/forms.py
@@ -1,9 +1,19 @@
 from django import forms
 
-class ReviewFrom(forms.Form):
+from .models import Review
+
+""" class ReviewFrom(forms.Form):
     user_name = forms.CharField(label="Your Name", max_length=100, error_messages={
         "required": "Your name must not be empty",
         "max_length":"Plase enter a shorter name!",
     })
     review_text = forms.CharField(label="Your Feedback", widget=forms.Textarea, max_length=200)
     rating = forms.IntegerField(label="Your Rating", min_value=1, max_value=5)
+ """
+
+class ReviewFrom(forms.ModelForm):
+    class Meta:
+        model = Review
+        fields = '__all__'
+
+    
