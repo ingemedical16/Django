f8533f9a873d2ec3c5e5167ce357193caac0932f
Mohammed El-IDRISSI <ingmedical16@gmail.com>
2025-09-14 14:33:50 +0000

    Saving Data with a Modelform

--- 010_Forms/reviews/views.py
+++ 010_Forms/reviews/views.py
@@ -8,11 +8,7 @@ def review(request):
     if request.method == 'POST':
        form = ReviewFrom(request.POST)
        if form.is_valid():
-            review = Review(
-                user_name = form.cleaned_data['user_name'], 
-                review_text = form.cleaned_data['review_text'], 
-                rating = form.cleaned_data['rating'])
-            review.save()
+            form.save()
             return redirect('thank_you')
     else:   
         form = ReviewFrom()
