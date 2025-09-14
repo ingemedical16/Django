1740ef3a921f4c25718f1826a64ca13d6fc3b700
Mohammed El-IDRISSI <ingmedical16@gmail.com>
2025-09-14 11:41:07 +0000

    Manual Form Validation & the Problems with "that"

--- 010_Forms/reviews/views.py
+++ 010_Forms/reviews/views.py
@@ -6,9 +6,15 @@ def review(request):
     if request.method == 'POST':
         entered_username =request.POST['username']
         print(entered_username)
+        if entered_username == "":
+            return render(request,"reviews/review.html",{
+        "has_error":True
+    })
         return redirect('thank_you')
     
-    return render(request,"reviews/review.html")
+    return render(request,"reviews/review.html",{
+        "has_error":False
+    })
 
 def thank_you(request):
     return render(request,"reviews/thank_you.html")
