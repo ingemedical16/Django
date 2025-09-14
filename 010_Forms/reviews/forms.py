7c31b274a53a0bd8229b158c2de3404d72840498
Mohammed El-IDRISSI <ingmedical16@gmail.com>
2025-09-14 12:54:55 +0000

    Customizing the Rendered HTML

--- 010_Forms/reviews/forms.py
+++ 010_Forms/reviews/forms.py
@@ -3,5 +3,5 @@ from django import forms
 class ReviewFrom(forms.Form):
     user_name = forms.CharField(label="Your Name", max_length=100, error_messages={
         "required": "Your name must not be empty",
-        "max_length":"Plase enter a shorter name!"
+        "max_length":"Plase enter a shorter name!",
     })
