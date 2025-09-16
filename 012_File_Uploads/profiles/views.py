0f020db4683f5da32341da538b43608c8abfe104
Mohammed EL-IDRISSI <ingmedical16@gmail.com>
2025-09-16 11:36:00 +0200

    Using a CreateView

--- 012_File_Uploads/profiles/views.py
+++ 012_File_Uploads/profiles/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponseRedirect
+from django.views.generic.edit import CreateView
 
 from .forms import ProfileForm
 from .models import UserProfile
@@ -8,17 +9,9 @@ from .models import UserProfile
 # Create your views here.
 
 
+class CreateProfileView(CreateView):
+    model = UserProfile
+    fields = '__all__'
+    template_name = "profiles/create_profile.html"
+    success_url = "/profiles"
 
-class CreateProfileView(View):
-    def get(self, request):
-        form = ProfileForm()
-        return render(request, "profiles/create_profile.html", {"form": form})
-
-    def post(self, request):
-        submitted_form = ProfileForm(request.POST, request.FILES)
-        if  submitted_form.is_valid():
-            profile = UserProfile(image=request.FILES['user_image'])
-            profile.save()
-            return HttpResponseRedirect("/profiles")
-        return render(request, "profiles/create_profile.html", {"form": submitted_form})
-        
