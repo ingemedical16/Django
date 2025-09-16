de060be458678c4eb58b5367ff86ed564f5c1456
Mohammed EL-IDRISSI <ingmedical16@gmail.com>
2025-09-16 10:03:51 +0200

    fix some issue

--- 011_Class_Views/reviews/models.py
+++ 011_Class_Views/reviews/models.py
@@ -5,5 +5,5 @@ from django.db import models
 class Review(models.Model):
     user_name = models.CharField(max_length=100)
     review_text = models.TextField(max_length=200)
-    rating = models.IntegerField(max_length=5)
+    rating = models.IntegerField(default=1)
 
