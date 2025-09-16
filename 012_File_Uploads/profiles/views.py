347b079c5ae00a48d33e29a1096a334292ff067d
Mohammed EL-IDRISSI <ingmedical16@gmail.com>
2025-09-16 11:48:32 +0200

    Working with the File Field

--- 012_File_Uploads/profiles/views.py
+++ 012_File_Uploads/profiles/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponseRedirect
 from django.views.generic.edit import CreateView
+from django.views.generic import ListView
 
-from .forms import ProfileForm
+
 from .models import UserProfile
 
 # Create your views here.
@@ -14,4 +15,9 @@ class CreateProfileView(CreateView):
     fields = '__all__'
     template_name = "profiles/create_profile.html"
     success_url = "/profiles"
+    
+class ProfilesListView(ListView):
+    model = UserProfile
+    template_name = "profiles/user_profiles.html"
+    context_object_name = "profiles"
 
