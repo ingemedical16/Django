9f5a6ae32552a3bb16b49e752df87f1bbda362c6
Mohammed EL-IDRISSI <ingmedical16@gmail.com>
2025-09-16 10:55:50 +0200

    Storing Uploaded Files Naive Approach

--- 012_File_Uploads/profiles/views.py
+++ 012_File_Uploads/profiles/views.py
@@ -3,12 +3,15 @@ from django.views import View
 from django.http import HttpResponseRedirect
 
 # Create your views here.
-
+def store_file(file):
+    with open("profiles/static/profiles/images/" + file.name, "wb+") as destination:
+        for chunk in file.chunks():
+            destination.write(chunk)
 
 class CreateProfileView(View):
     def get(self, request):
         return render(request, "profiles/create_profile.html")
 
     def post(self, request):
-        print(request.FILES['image'])
+        store_file(request.FILES['image'])
         return HttpResponseRedirect("/profiles")
