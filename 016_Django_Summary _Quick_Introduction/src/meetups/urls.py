999aef7de1a6d49af161f8f8d674b954f513adc6
Mohammed El-IDRISSI <ingmedical16@gmail.com>
2025-09-28 14:29:27 +0000

    Dynamic Paths

--- 016_Django_Summary _Quick_Introduction/src/meetups/urls.py
+++ 016_Django_Summary _Quick_Introduction/src/meetups/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import index
+from .views import index, meetup_details
 
 urlpatterns = [
-    path("meetups/", index, name="index"), # our-domain.com/meetups/
+    path("meetups/", index, name="index"), # our-domain.com/meetups
+    path("meetups/<slug:meetup_slug>/", meetup_details, name="meetup-details"), # our-domain.com/meetups/a-first-meetup   
 ]
