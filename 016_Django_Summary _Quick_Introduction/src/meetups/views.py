a9d50ff802d3f4c84d3205b3bdb70cc88c485df5
Mohammed El-IDRISSI <ingmedical16@gmail.com>
2025-09-28 13:25:53 +0000

    Key Django Template Language Features & Tags

--- 016_Django_Summary _Quick_Introduction/src/meetups/views.py
+++ 016_Django_Summary _Quick_Introduction/src/meetups/views.py
@@ -4,4 +4,11 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, 'meetups/index.html')
+    meetups =[
+        {'title': 'A First Meetup', 'location': 'New York', 'slug': 'a-first-meetup'},
+        {'title': 'A Second Meetup', 'location': 'New York', 'slug': 'a-second-meetup'},
+        {'title': 'A Third Meetup', 'location': 'New York', 'slug': 'a-third-meetup'},  
+    ]
+    return render(request, 'meetups/index.html', {
+        'meetups': meetups,
+    })
