456937b8f74f116de329559b3113178a058dd20d
Mohammed EL-IDRISSI <ingmedical16@gmail.com>
2025-09-30 11:40:07 +0200

    Querying Data

--- 016_Django_Summary _Quick_Introduction/src/meetups/views.py
+++ 016_Django_Summary _Quick_Introduction/src/meetups/views.py
@@ -1,23 +1,25 @@
 from django.shortcuts import render
 
+from .models import Meetup
 
 # Create your views here.
 
 def index(request):
-    meetups =[
-        {'title': 'A First Meetup', 'location': 'New York', 'slug': 'a-first-meetup'},
-        {'title': 'A Second Meetup', 'location': 'New York', 'slug': 'a-second-meetup'},
-        {'title': 'A Third Meetup', 'location': 'New York', 'slug': 'a-third-meetup'},  
-    ]
+    meetups = Meetup.objects.all()
     return render(request, 'meetups/index.html', {
         'meetups': meetups,
     })
 def meetup_details(request, meetup_slug): 
-    slected_meetup = {
-        'title': 'A First Meetup',
-        'description': 'This is a first meetup description',
-    }   
-    return render(request, 'meetups/meetup_details.html', {
-        'meetup_title': slected_meetup['title'],
-        'meetup_description': slected_meetup['description'],
-    })
+    try:
+        selected_meetup = Meetup.objects.get(slug=meetup_slug)
+        return render(request, 'meetups/meetup-details.html', {
+            'meetup': selected_meetup,
+            'title': selected_meetup.title,
+            'description': selected_meetup.description,
+        })
+    except Exception as e:
+        print(e)
+        return render(request, 'meetups/meetup-details.html', {
+            'meetup': None,
+            'title': 'Meetup Not Found',
+        })  
